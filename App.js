2a0513283c4d4a24cc767f589ea2e00b9e539123
Rogerio Prado <90151800+RogerioPradoo@users.noreply.github.com>
2023-11-10 20:55:55 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,16 +2,6 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/pages/rotas/rotas';
-// import Principal from '../principal/index'
-// import Login from '../login/index';
-// import Inicio from '../inicio/index'
-// import Criar from '../criarConta/index'
-// import Filmes from '../filmes/index'
-// import Series from '../series/index'
-// import Lista from '../lista/index.js'
-// import Contas from '../trocarConta/index'
-// import Recuperar from '../recuperar/index'
-//import { Provider as PaperProvider } from 'react-native-paper'
 
 const App = () => {
   return (
